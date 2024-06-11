1938eee2edf4fc26c528f1046515f1ba7a8bddad
priyanka-deshmukh8 <priyankadeshmukh.h@gmail.com>
2024-06-12 00:38:27 +0530

    Added

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
